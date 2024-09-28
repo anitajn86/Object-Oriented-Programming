10646f67a895113adf3dffc768c4037ec51c13d9
Anita <anitajn7990@gmail.com>
2024-09-28 23:08:40 +0300

    Trying out different projects

---
+++ coffeemachine1.py
@@ -0,0 +1,6 @@
+#1.Prompt the user by asking what they would like? (espresso,latte,cappuccino)
+#a.Check the user's input to decide what to do next.
+#b.The prompt should show every time action has completed. e.g once the drink is dispensed,
+#the prompt should show again to serve the next customer
+
+customer_choice=input("What would you like? (espresso, latte, cappuccino):")
---
+++ library.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self,title,author,isbn,status):
+        self.title=title
+        self.author=author
+        self.isbn=isbn
+        self.status=status
+        
