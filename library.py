446b44f93447744a51b439a0c4a5c08aef6e3db7
Anita <anitajn7990@gmail.com>
2024-09-28 23:37:34 +0300

    classes

--- library.py
+++ library.py
@@ -1,7 +1,22 @@
 class Book:
-    def __init__(self,title,author,isbn,status):
+    def __init__(self,title,author,isbn):
         self.title=title
         self.author=author
         self.isbn=isbn
-        self.status=status
-        
+        self.status="available"
+        print(f"{self.title} by {self.author} (IsbN{self.isbn}) is {self.status}")
+
+class Member:
+    def __init__(self,name,member_id):
+        self.name=name
+        self.member_id=member_id
+        self.borrowed_books=[]
+
+    def borrowBook(self,book):
+        if book.status=="available":
+            self.borrowed_books=self.borrowed_books.append(book)
+            book.status="borrowwed"
+            print(f"{self.name} has borrowed {book.title}")
+        else:
+            print(f"Sorry, {book.title} is already borrowed")
+
---
+++ librarytoo.py
@@ -0,0 +1,2 @@
+from library import *
+Book1=Book("Melanin Queen","Anita", 1001)
