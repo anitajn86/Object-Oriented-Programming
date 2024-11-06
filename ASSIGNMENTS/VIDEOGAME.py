7ec0195def57d783ed10b4dc8e4edc43ebda067d
Anita <anitajn7990@gmail.com>
2024-11-06 14:18:45 +0300

    The video game

--- ASSIGNMENTS/VIDEOGAME.py
+++ ASSIGNMENTS/VIDEOGAME.py
@@ -1,91 +1,116 @@
 # Character class with encapsulation
 class Character:
     def __init__(self, name, health, position):
-        self._name = name
-        self._health = health
-        self._position = position
+        self.__name = name
+        self.__health = health
+        self.__position = position
 
     def move(self, new_position):
-        self._position = new_position
-        print(f"{self._name} moves to position {self._position}")
+        self.__position = new_position
+        print(f"{self.__name} moves to position {self.__position}")
 
-    def attack(self):
-        print(f"{self._name} attacks!")
+    def attack(self,target):
+        if self.__position==target.__position:
+            print(f"{self.__name} attacks {target}")
+        else:
+            print(f"{target} is too far.")
 
-    def interact(self):
-        print(f"{self._name} interacts with the environment.")
+    def condition(self,new_healthStatus):
+        self.__health=new_healthStatus
+        print(f"{self.__name} is {self.__health}")
+
+    #adding a method in the character class to extend its functionality
+    def use_vehicle(self, vehicle, destination):
+        """Boards a vehicle, drives it to a destination, and exits upon arrival."""
+        if vehicle:
+            self.__vehicle = vehicle
+            print(f"{self.__name} boards the {self.__vehicle}")
+            self.__vehicle.drive(destination)
+            self.__position = destination
+            print(f"{self.__name} arrives at position {self.__position} in the {self.__vehicle}.")
+            self.__vehicle = None
+        else:
+            print(f"{self.__name} is already using a vehicle.")
+
+# C1 = Character("Megamind", 100, (3, 0))
+# C2 = Character("Batman", 100, (5, 1))
+# # Character moves and attacks
+# C1.move((5, 5))
+# #C1.attack()
+# C2.move((6,0))
+# C2.attack(C1)
+# C2.condition("Alive and well")
+# C1.move((6,0))
+# C2.attack(C1)
+# C1.condition("Injured")
+C3 = Character("Miles Morales", 200, (7,4))
 
 # Vehicle class with encapsulation
 class Vehicle:
     def __init__(self, type, speed, fuel_level):
-        self._type = type
-        self._speed = speed
-        self._fuel_level = fuel_level
+        self.__type = type
+        self.__speed = speed
+        self.__fuel_level = fuel_level
 
-    def drive(self):
-        if self._fuel_level > 0:
-            print(f"The {self._type} drives at speed {self._speed}.")
-            self._fuel_level -= 10
+    def drive(self,destination):
+        if self.__fuel_level > 0:
+            print(f"The {self.__type} drives at speed {self.__speed} to {destination}.")
+            self.__fuel_level -= 10
         else:
-            print(f"The {self._type} is out of fuel!")
+            print(f"The {self.__type} is out of fuel!")
 
     def refuel(self):
-        self._fuel_level = 100
-        print(f"The {self._type} is refueled to {self._fuel_level}.")
+        self.__fuel_level = 100
+        print(f"The {self.__type} is refueled to {self.__fuel_level}.")
 
     def stop(self):
-        print(f"The {self._type} stops.")
+        print(f"The {self.__type} stops.")
 
-# Extending Character class to interact with Vehicle
-class CharacterWithVehicle(Character):
-    def get_in(self, vehicle):
-        self._vehicle = vehicle
-        print(f"{self._name} gets into the {self._vehicle._type}.")
+V1= Vehicle("Car", 60, 50)
+V2= Vehicle("Bike", 120, 70)
+V1.drive((2,1))
+V1.refuel()
+V1.stop()
+V2.drive((6,9))
+V2.refuel()
+V2.stop()
 
-    def get_out(self):
-        print(f"{self._name} gets out of the {self._vehicle._type}.")
-        self._vehicle = None
+C3.use_vehicle("Bike",(10,1))
 
-# Specialized Character class using inheritance
-class HeroCharacter(Character):
+#Special characters inheriting from the character class
+class UltimateHero(Character):
     def double_jump(self):
-        print(f"{self._name} performs a double jump!")
+        print(f"{self.__name} performs a double jump!")
 
     def fast_run(self):
-        print(f"{self._name} runs fast!")
+        print(f"{self.__name} runs fast!")
 
-# Polymorphic behavior
-def handle_object(obj):
-    if isinstance(obj, Character):
-        obj.interact()
-    elif isinstance(obj, Vehicle):
-        obj.drive()
 
 # Simple game scenario
-def game_scenario():
-    # Create character and vehicle objects
-    character = Character("Player1", 100, (0, 0))
-    hero = HeroCharacter("Hero", 150, (10, 10))
-    vehicle = Vehicle("Car", 60, 50)
+# def game_scenario():
+#     # Create character and vehicle objects
+#     character = Character("Player1", 100, (0, 0))
+#     hero = HeroCharacter("Hero", 150, (10, 10))
+#     vehicle = Vehicle("Car", 60, 50)
     
-    # Character moves and interacts
-    character.move((5, 5))
-    character.attack()
-    character.interact()
+#     # Character moves and interacts
+#     character.move((5, 5))
+#     character.attack()
+#     character.interact()
 
-    # Character interacts with vehicle
-    player_with_vehicle = CharacterWithVehicle("Player2", 100, (0, 0))
-    player_with_vehicle.get_in(vehicle)
-    vehicle.drive()
-    player_with_vehicle.get_out()
+#     # Character interacts with vehicle
+#     player_with_vehicle = CharacterWithVehicle("Player2", 100, (0, 0))
+#     player_with_vehicle.get_in(vehicle)
+#     vehicle.drive()
+#     player_with_vehicle.get_out()
 
-    # Hero character uses specialized abilities
-    hero.double_jump()
-    hero.fast_run()
+#     # Hero character uses specialized abilities
+#     hero.double_jump()
+#     hero.fast_run()
 
-    # Handling different object types with polymorphism
-    handle_object(character)
-    handle_object(vehicle)
+#     # Handling different object types with polymorphism
+#     handle_object(character)
+#     handle_object(vehicle)
 
-# Run the game scenario
-game_scenario()
+# # Run the game scenario
+# game_scenario()
